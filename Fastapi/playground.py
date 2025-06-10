ed73c2c05000e9a24475ae68753cc253c9253d15
Julen <julenbarriga@gmail.com>
2025-06-10 19:14:35 +0200

    'update'

--- Fastapi/playground.py
+++ Fastapi/playground.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI 
-from pydantic import BaseModel
-from typing import Optional
 from classes_values import Identity
-
+import pandas as pd
 
 app = FastAPI()
 
@@ -13,3 +11,15 @@ def roout(name: str):
 @app.post('/test')
 def testing(info: Identity): 
     return {'Message': f'Hola me llamo {info.name} y mi telefono es {info.phone_number}'}
+
+@app.get('/read_dataframe')
+def read_dataframe_properties(): 
+    df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
+    value = df["sepal_length"]
+    return {'Value': value}
+
+@app.get('/read_dataframe/{position}')
+def read_dataframe_properties(position: int): 
+    df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
+    value = df["sepal_length"][position]
+    return {'Value': value}
